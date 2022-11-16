047ea8f7e7fd6dd7b813fbc3fd7132da178406bb
minhtawng <minhtangquoc@gmail.com>
2022-11-16 10:21:20 +0700

    8

--- config/db/index.js
+++ config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect(){
     try{
-        await mongoose.connect('mongodb+srv://admin:minhkhongngu@cluster1.qh46kp8.mongodb.net/minhkhon');
+        await mongoose.connect('mongodb+srv://admin:minh8882@cluster1.qh46kp8.mongodb.net/minhkhon');
         console.log("Connect to DB successfully!");
     } catch (error){
         console.log(error);
